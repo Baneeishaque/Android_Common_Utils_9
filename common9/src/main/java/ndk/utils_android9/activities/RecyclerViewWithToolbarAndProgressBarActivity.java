45056935969a39f07597a4367079b51efb097e4e
Banee Ishaque K <baneeishaque@gmail.com>
2020-07-09 20:28:58 +0530

    search

--- common9/src/main/java/ndk/utils_android9/activities/RecyclerViewWithToolbarAndProgressBarActivity.java
+++ common9/src/main/java/ndk/utils_android9/activities/RecyclerViewWithToolbarAndProgressBarActivity.java
@@ -1,11 +1,19 @@
 package ndk.utils_android9.activities;
 
+import android.app.SearchManager;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.util.Pair;
+import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -79,4 +87,58 @@ public abstract class RecyclerViewWithToolbarAndProgressBarActivity extends Acti
     }
 
     public abstract void prepareRecyclerView();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+        SearchManager searchManager = (SearchManager) this.getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager != null ? searchManager.getSearchableInfo(this.getComponentName()) : null);
+
+        EditText searchEdit = searchView.findViewById(androidx.appcompat.R.id.search_src_text);
+        searchEdit.setTextColor(Color.WHITE);
+        searchEdit.setHintTextColor(Color.WHITE);
+        searchEdit.setBackgroundColor(Color.TRANSPARENT);
+        searchEdit.setHint("Search");
+
+        InputFilter[] fArray = new InputFilter[2];
+        fArray[0] = new InputFilter.LengthFilter(40);
+        fArray[1] = (source, start, end, dest, destination_start, destination_end) -> {
+
+            for (int i = start; i < end; i++) {
+
+                if (!Character.isLetterOrDigit(source.charAt(i)))
+                    return "";
+            }
+
+            return null;
+        };
+
+        searchEdit.setFilters(fArray);
+
+        View v = searchView.findViewById(androidx.appcompat.R.id.search_plate);
+        v.setBackgroundColor(Color.TRANSPARENT);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String searchKey) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String searchKey) {
+
+                return configureSearchAction(searchKey);
+            }
+        });
+
+        return true;
+    }
+
+    public abstract boolean configureSearchAction(String searchKey);
 }
